e6c747a9829b7b3ee0046426525d678e0b55acb7
TIMOTHY ANDES <tim.andes+git@gmail.com>
2024-11-26 15:28:59 -0700

    add pseudo code

---
+++ bprp-main.py
@@ -0,0 +1,19 @@
+"""
+A poll...
+Everyone responding to this poll chooses between a blue or red pill.
+
+- if > 50% of people choose the blue pill, everyone lives
+- if not, red pills live and blue pills die
+
+In other words, if more than 50% of people choose the blue pill everyone lives.
+If less than or equal to 50% choose the blue pill, everyone dies.
+
+Blue pill > 50%: everyone lives
+Blue pill <= 50%, blues die
+"""
+
+# randomly generate an array of color blue, and color red as strings
+## blue = 0, red = 1
+## calculate % type(float) blue: red / blue 
+# take data array in arg 
+# calculate % red and % blue

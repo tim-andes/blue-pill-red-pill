636a99386595a2a6be80a266c24b65b1de15a0a2
TIMOTHY ANDES <tim.andes+git@gmail.com>
2024-11-27 15:21:58 -0700

    create functions and tests

--- bprp.py
+++ bprp.py
@@ -11,9 +11,36 @@ If less than or equal to 50% choose the blue pill, everyone dies.
 Blue pill > 50%: everyone lives
 Blue pill <= 50%, blues die
 """
+import random as random
 
 # randomly generate an 100 count array of color blue, and color red as strings
 ## blue = 0, red = 1
 ## calculate % type(float) blue: red / blue 
 # take data array in arg 
 # calculate % red and % blue
+
+def generate_zero_one():
+    return int(random.randint(0,1))
+
+def generate_array():
+    sample = [generate_zero_one() for i in range(10000)]
+    return sample
+
+def tally():
+    tally_sample = generate_array()
+    tally_list = [0, 0]
+    
+    for number in tally_sample:
+        if number == 0:
+            tally_list[0] += 1
+        else:
+            tally_list[1] += 1
+    return tally_list
+
+
+
+def main():
+    return 
+
+if __name__ == "__main__":
+    main()
--- test-bprp-main.py
+++ test-bprp-main.py
@@ -1,49 +1,54 @@
 import unittest
+import bprp
 
-# test array
+
 # test that elements are 0 or 1
 # test that elements are type(int) and not floats w/ decimals
-
-class TestArray(unittest.TestCase):
-    """
-    Tests for arithmetic.
-    """
-    def test_generate_array(self):
-        """
-        Tests the `generate_array` function.
-        - Check type(array)
-        - Check that elements are type(int)
-        - Check len(array) is 10,000
-
-        """
-        # self.assertEqual(add(2,3), 5)
-
-class TestRandomNumberGenerator(unittest.TestCase):
-    """
-    Tests random
-    """
+class TestBprp(unittest.TestCase):
     def test_generate_zero_one(self):
         """
         Tests the `generate_zero_one` function.
         - Check that all numbers are type(int)
         - Check that all numbers are either 0 or 1
         """
-        # self.assertEqual(add(2,3), 5)
+        self.assertLessEqual(bprp.generate_zero_one(), 1)
+        self.assertGreaterEqual(bprp.generate_zero_one(), 0)
+        self.assertIsInstance(bprp.generate_zero_one(), int)
 
-# test percentage calculation
-class TestCalculateBluePercentage(unittest.TestCase):
-    """
-    Tests random
-    """
-    def test_calculate_percent_blue(self):
+    def test_generate_array(self):
         """
-        Tests the `calculate_percent_blue` function.
-        - 
-        - Check that all numbers are either 0 or 1
+        Tests the `generate_array` function.
+        - Check type(array)
+        - Check len(array) is 10,000
         """
-        # self.assertEqual(add(2,3), 5)
+        self.assertIsInstance(bprp.generate_array(), list)
+        self.assertEqual(len(bprp.generate_array()), 10000)
+
+    def test_tally(self):
+        """
+        Tests the `tally` function which outputs a 2 element array, where [# 0's, # 1's]
+        - Check type(array)
+        - Check that tally array length is 2
+        - Check that elements are both type(int)
+        """
+        self.assertIsInstance(bprp.tally(), list)
+        self.assertEqual(len(bprp.tally()), 2)
+
+    def test_percentage(self):
+                
+
+    # def test_calculate_percent_blue(self):
+    #     """
+    #     Tests the `calculate_percent_blue` function.
+    #     - 
+    #     - Check that all numbers are either 0 or 1
+    #     """
+    #     # self.assertEqual(add(2,3), 5)
+
 # percentage will be int during count
-
 # test output
 # percentage will convert to float with two decimal places
-# test output red or blue
+# test output red or bluetest=
+
+if __name__ == "__main__":
+    unittest.main()
